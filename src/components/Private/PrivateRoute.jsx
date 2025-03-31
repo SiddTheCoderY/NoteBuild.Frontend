9deb9cf0b2c4292a18c5f86da1b64515217ff169
SiddTheCoderY <siddhant23sci180@xa.edu.np>
2025-03-31 19:05:05 +0545

    Update PrivateRoute.jsx

--- src/components/Private/PrivateRoute.jsx
+++ src/components/Private/PrivateRoute.jsx
@@ -11,7 +11,7 @@ const PrivateRoute = () => {
     const checkAuth = async () => {
       try {
         console.log('Sending request to verify user')
-        const response = await axios.get('http://localhost:8000/api/auth/verifyUser', {
+        const response = await axios.get('https://notebuild-backend.onrender.com/api/auth/verifyUser', {
           withCredentials: true,  
         });
         console.log('Request ACcepted For Verificaton of user' , response)
